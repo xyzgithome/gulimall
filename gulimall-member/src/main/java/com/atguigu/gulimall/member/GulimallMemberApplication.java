b081a2b7a9ac4f9ea86fc48ebaf2e6a69cbdb839
xyzgithome <2584118953@qq.com>
2023-07-03 22:41:35 +0800

    nacos配置中心使用测试

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * 1、想要远程调用别的服务
+ * 1）、引入open-feign
+ * 2）、编写一个接口，告诉SpringCloud这个接口需要调用远程服务
+ *   1、声明接口的每一个方法都是调用哪个远程服务的那个请求
+ * 3）、开启远程调用功能
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.atguigu.gulimall.member.fegin")
