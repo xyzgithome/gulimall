57a8756b61c7fd871a2973f9e2531d48ae4d8c86
xyzgithome <2584118953@qq.com>
2023-07-10 00:44:04 +0800

    拖拽功能完成

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/CategoryController.java
@@ -45,7 +45,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId){
 		CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
@@ -61,7 +61,16 @@ public class CategoryController {
     /**
      * 修改
      */
-    @RequestMapping("/update")
+    @RequestMapping(value = "/update/sort", method = RequestMethod.POST)
+    public R updateSort(@RequestBody CategoryEntity[] categories){
+        categoryService.updateBatchById(Arrays.asList(categories));
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
     public R update(@RequestBody CategoryEntity category){
 		categoryService.updateById(category);
 
