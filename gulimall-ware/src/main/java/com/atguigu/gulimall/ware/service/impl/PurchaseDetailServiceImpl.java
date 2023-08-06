ac054c5084f6a5c5194972300ec0a8f3811afcdd
xyzgithome <2584118953@qq.com>
2023-08-07 07:44:22 +0800

    库存系统-采购单维护-采购需求-分页查询

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/PurchaseDetailServiceImpl.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -1,5 +1,6 @@
 package com.atguigu.gulimall.ware.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,9 +19,36 @@ public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, Pu
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        /**
+         * status: 0,//状态
+         *    wareId: 1,//仓库id
+         */
+
+        QueryWrapper<PurchaseDetailEntity> queryWrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if(!StringUtils.isEmpty(key)){
+            //purchase_id  sku_id
+            queryWrapper.and(w->{
+                w.eq("purchase_id",key).or().eq("sku_id",key);
+            });
+        }
+
+        String status = (String) params.get("status");
+        if(!StringUtils.isEmpty(status)){
+            //purchase_id  sku_id
+            queryWrapper.eq("status",status);
+        }
+
+        String wareId = (String) params.get("wareId");
+        if(!StringUtils.isEmpty(wareId)){
+            //purchase_id  sku_id
+            queryWrapper.eq("ware_id",wareId);
+        }
+
         IPage<PurchaseDetailEntity> page = this.page(
                 new Query<PurchaseDetailEntity>().getPage(params),
-                new QueryWrapper<PurchaseDetailEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
