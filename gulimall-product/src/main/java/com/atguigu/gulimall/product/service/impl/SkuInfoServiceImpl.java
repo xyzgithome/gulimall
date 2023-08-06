4e79630c67984f0053d851a52329135c7f876c36
xyzgithome <2584118953@qq.com>
2023-08-06 22:06:23 +0800

    商品系统-商品维护-商品（sku）管理-查询功能后端接口优化

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SkuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,7 +1,11 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -26,4 +30,52 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
         return new PageUtils(page);
     }
 
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> queryWrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if (StringUtils.isNotBlank(key)) {
+            queryWrapper.and((w) -> {
+                w.eq("sku_id", key).or().like("sku_name", key);
+            });
+        }
+
+        String categoryId = (String) params.get("catelogId");
+        if (StringUtils.isNotBlank(categoryId) && !StringUtils.equalsIgnoreCase("0", categoryId)) {
+            queryWrapper.eq("catalog_id", categoryId);
+        }
+
+        String brandId = (String) params.get("brandId");
+        if (StringUtils.isNotBlank(brandId) && !StringUtils.equalsIgnoreCase("0", brandId)) {
+            queryWrapper.eq("brand_id", brandId);
+        }
+
+        String min = (String) params.get("min");
+        if (StringUtils.isNotBlank(min)) {
+            queryWrapper.ge("price", min);
+        }
+
+        String max = (String) params.get("max");
+        if (isLegalMaxPrice(max)) {
+            queryWrapper.le("price", max);
+        }
+
+        IPage<SkuInfoEntity> page = this.page(
+                new Query<SkuInfoEntity>().getPage(params),
+                queryWrapper
+        );
+        return new PageUtils(page);
+    }
+
+    private boolean isLegalMaxPrice(String maxPrice) {
+        if (StringUtils.isBlank(maxPrice)) {
+            return false;
+        }
+        try {
+            return new BigDecimal(maxPrice).compareTo(new BigDecimal("0")) >= 1;
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("最大价格转换异常");
+        }
+    }
 }
